dcc816a36de0acfb97f117de15450c9e1cbb75b8
unknown <897418436@qq.com>
2018-06-27 21:33:48 +0800

    fix bug

--- src/index.js
+++ src/index.js
@@ -24,31 +24,48 @@ function Square(props) {
   );
 }
   class Board extends React.Component {
+    constructor() {
+      super();
+      this.lines1 = [];
+      this.lines2 = [];
+      this.lines3 = [];
+    }
     renderSquare(i) {
+      let win = this.props.winlines.indexOf(i) !== -1 ? 'win': '';
       return (
         <Square 
-          value={this.props.squares[i]}
+          key={i}
+          value={this.props.squares[i] + ' ' + win}
           onClick={() => this.props.onClick(i)}
         />
       );
     }
     render() {
+      let lines = {
+        lines1: [],
+        lines2: [],
+        lines3: [],
+      }
+      let z = 0;
+      for(let i = 0; i<= 2; i++) {
+        for (let j = 0; j <= 2; j++) {
+          lines['lines' + (i + 1)].push(this.renderSquare(z));
+          z+=1;
+        }
+      }
+      Object.keys(lines).forEach((val, key) => {
+        this[val] = lines[val];
+      });
       return (
         <div>
           <div className="board-row">
-            {this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
+           {this.lines1}
           </div>
           <div className="board-row">
-            {this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
+            {this.lines2}
           </div>
           <div className="board-row">
-            {this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
+            {this.lines3}
           </div>
         </div>
       );
@@ -91,26 +108,46 @@ function Square(props) {
         xIsNext: (step % 2) ? false : true,
       });
     }
+    prevAct() {
+      let step = this.state.stepNumber === 0 ? 0 : this.state.stepNumber - 1;
+      this.setState({
+        stepNumber: step,
+        xIsNext: (step % 2) ? false : true,
+      });
+    }
+    nextAct() {
+      let num = this.state.stepNumber;
+      let step = num === this.state.history.length - 1 ? this.state.history.length - 1 : num + 1;
+      this.setState({
+        stepNumber: step,
+        xIsNext: (step % 2) ? false : true,
+      });
+    }
+    resetAct() {
+      this.setState({
+        stepNumber: 0,
+        xIsNext: true,
+      });
+    }
     render() {
       const history = this.state.history;
       const current = history[this.state.stepNumber];
-      const winner = calculateWinner(current.squares);
+      const result = calculateWinner(current.squares);
+      const winlines = result ? result.lines : [];
       const moves = history.map((step, move) => {
         const desc = move ?
           'Move #' + move :
           'Game start';
+        const currentAct = move === this.state.stepNumber ? 'currentAct' : ''; 
         return (
           <li key={move}>
-            <label href="#" onClick={() => this.jumpTo(move)}>{desc}</label>
+            <label className={currentAct} href="#" onClick={() => this.jumpTo(move)}>{desc}</label>
           </li>
         );
       });
       let status;
-      if (winner) {
-        status = 'Winner: ' + (winner === 'cha' ? '黑棋' : '白棋');
-        setTimeout(() => {
-          alert('胜利者 -> '+ (winner === 'cha' ? '黑棋' : '白棋'));
-        }, 100)
+      if (result) {
+        status = 'Winner: ' + (result.winner === 'cha' ? '黑棋' : '白棋');
       } else {
         status = 'Next player: ' + (this.state.xIsNext ? '黑棋' : '白棋');
       }
@@ -121,10 +158,16 @@ function Square(props) {
             <Board
               squares={current.squares}
               onClick={(i) => this.handleClick(i)}
+              winlines = {winlines}
             />
           </div>
           <div className="game-info">
             <div>{status}</div>
+            <div className="operate">
+              <button onClick={(i) => this.prevAct()}>上一步</button>
+              <button onClick={(i) => this.resetAct()}>刷新</button>
+              <button onClick={(i) => this.nextAct()}>下一步</button>
+            </div>
             <ol>{moves}</ol>
           </div>
         </div>
@@ -153,7 +196,10 @@ function Square(props) {
     for (let i = 0; i < lines.length; i++) {
       const [a, b, c] = lines[i];
       if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-        return squares[a];
+        return {
+          winner:squares[a],
+          lines: lines[i],
+        };
       }
     }
     return null;
