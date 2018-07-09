8d7ead2d36a5dfcc6196c3967cb55706edf7a0d2
unknown <897418436@qq.com>
2018-07-09 20:19:32 +0800

    add temperature

--- src/index.js
+++ src/index.js
@@ -273,3 +273,196 @@ function Square(props) {
     <IllegalButton text="hello world!!" />,
     document.getElementById('illegal')
   );
+
+  //====================
+  const scaleNames = {
+    c: 'Celsius',
+    f: 'Fahrenheit'
+  };
+  
+  function toCelsius(fahrenheit) {
+    return (fahrenheit - 32) * 5 / 9;
+  }
+  
+  function toFahrenheit(celsius) {
+    return (celsius * 9 / 5) + 32;
+  }
+  
+  function tryConvert(temperature, convert) {
+    const input = parseFloat(temperature);
+    if (Number.isNaN(input)) {
+      return '';
+    }
+    const output = convert(input);
+    const rounded = Math.round(output * 1000) / 1000;
+    return rounded.toString();
+  }
+  
+  function BoilingVerdict(props) {
+    if (props.celsius >= 100) {
+      return <p>The water would boil.</p>;
+    }
+    return <p>The water would not boil.</p>;
+  }
+  
+  class TemperatureInput extends React.Component {
+    constructor(props) {
+      super(props);
+      this.handleChange = this.handleChange.bind(this);
+    }
+  
+    handleChange(e) {
+      this.props.onTemperatureChange(e.target.value);
+    }
+  
+    render() {
+      const temperature = this.props.temperature;
+      const scale = this.props.scale;
+      return (
+        <fieldset>
+          <legend>Enter temperature in {scaleNames[scale]}:</legend>
+          <input value={temperature}
+                 onChange={this.handleChange} />
+        </fieldset>
+      );
+    }
+  }
+  
+  class Calculator extends React.Component {
+    constructor(props) {
+      super(props);
+      this.handleCelsiusChange = this.handleCelsiusChange.bind(this);
+      this.handleFahrenheitChange = this.handleFahrenheitChange.bind(this);
+      this.state = {temperature: '', scale: 'c'};
+    }
+  
+    handleCelsiusChange(temperature) {
+      this.setState({scale: 'c', temperature});
+    }
+  
+    handleFahrenheitChange(temperature) {
+      this.setState({scale: 'f', temperature});
+    }
+  
+    render() {
+      const scale = this.state.scale;
+      const temperature = this.state.temperature;
+      const celsius = scale === 'f' ? tryConvert(temperature, toCelsius) : temperature;
+      const fahrenheit = scale === 'c' ? tryConvert(temperature, toFahrenheit) : temperature;
+  
+      return (
+        <div>
+          <TemperatureInput
+            scale="c"
+            temperature={celsius}
+            onTemperatureChange={this.handleCelsiusChange} />
+          <TemperatureInput
+            scale="f"
+            temperature={fahrenheit}
+            onTemperatureChange={this.handleFahrenheitChange} />
+          <BoilingVerdict
+            celsius={parseFloat(celsius)} />
+        </div>
+      );
+    }
+  }
+  
+  ReactDOM.render(
+    <Calculator />,
+    document.getElementById('temptrue')
+  );
+
+  //=======================
+  class SearchBar extends React.Component{
+    constructor(props) {
+      super(props);
+    }
+    render() {
+      return (
+        <div>
+          <input value={this.props.value} onChange={this.props.onChange} />
+        </div>
+      )
+    }
+  }
+  class TableTd extends React.Component{
+    constructor(props) {
+      super(props);
+    }
+    render() {
+      return (
+        <div className="shopping-table-td">
+          {this.props.value}
+        </div>
+      )
+    }
+  }
+  class TableTr extends React.Component{
+    constructor(props) {
+      super(props);
+    }
+    render() {
+      let data = this.props.title.map((val) => {
+        return (
+          <TableTd value={val} />
+        )
+      });
+      return (
+        <div>
+          {data}
+        </div>
+      )
+    }
+  }
+  class TableTitle extends React.Component{
+    constructor(props) {
+      super(props);
+    }
+    render() {
+      return (
+        <div>
+          <TableTr title={this.props.title} />
+        </div>
+      )
+    }
+  }
+  class ProductTable extends React.Component{
+    constructor(props) {
+      super(props);
+    }
+    render() {
+      return (
+        <div>
+         <TableTitle title={this.props.title} />
+        </div>
+      )
+    }
+  }
+  class FilterableProductTable  extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        value: '',
+      }
+      this.handleValueChange = this.handleValueChange.bind(this);
+      this.title = ['Name', 'Price'];
+    }
+    handleValueChange(value) {
+      this.setState({
+        value: value
+      });
+    }
+    render() {
+      return (
+        <div className="shoppingList">
+          <SearchBar value={this.state.value} onChange={this.handleValueChange} />
+          <ProductTable title={this.title} />
+        </div>
+      )
+    }
+  }
+  ReactDOM.render(
+    <FilterableProductTable />,
+    document.getElementById('shopping')
+  );
+
